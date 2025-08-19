2988da3fd8c42a2a57a003fe70a26ff22ceaf783
jhonatanYK <jhonatan.kaneda@edu.unipar.br>
2025-08-18 22:29:02 -0300

    sdfsd

--- routes/livrosRoutes.js
+++ routes/livrosRoutes.js
@@ -4,6 +4,9 @@ const route = express.Router();
 const livrosController =
   require('../controllers/livrosController');
 
-route.get('/', livrosController.listar);
-
+  route.get('/', livrosController.listar);
+  route.get('/criar', livrosController.criar);   
+  route.post('/', livrosController.inserir);
+  route.get('/:id/editar', livrosController.editar);
+  route.post('/:id', livrosController.atualizar);
 module.exports = route;
